5e9dc695b277d346825777232f8b494b00f695ad
jacobdlewis <jacobdlewis@gmail.com>
2015-03-15 19:05:07 -0500

    game canvas displays on page

---
+++ app/js/main.js
@@ -0,0 +1,6 @@
+var game = new Phaser.Game(600, 400, Phaser.AUTO, 'game-holder',
+           { preload: preload, create: create, update: update });
+
+function preload() {}
+function create() {}
+function update() {}
