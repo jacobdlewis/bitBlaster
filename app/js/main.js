572a8494bc1a7732356ffa6c68b59cc15b46e72b
jacobdlewis <jacobdlewis@gmail.com>
2015-03-15 19:52:12 -0500

    starfield background appears on canvas

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,16 @@
+'use strict'
 var game = new Phaser.Game(600, 400, Phaser.AUTO, 'game-holder',
            { preload: preload, create: create, update: update });
 
-function preload() {}
-function create() {}
-function update() {}
+function preload() {
+
+  game.load.image('starfield', 'app/assets/starfield.png');
+}
+
+function create() {
+  game.add.sprite(0, 0, 'starfield');
+}
+
+function update() {
+
+}
