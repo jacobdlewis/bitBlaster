a6f84591939377ab72b90311766fb3db8049e945
jacobdlewis <jacobdlewis@gmail.com>
2015-03-17 09:40:47 -0500

    enemy ships grouped more tightly. score more appropriate

--- app/js/main.js
+++ app/js/main.js
@@ -48,9 +48,9 @@ function create() {
   baddie1.physicsBodyType = Phaser.Physics.ARCADE;
 
   for (var i = 0; i < 50; i++) {
-    var c = baddie1.create(game.world.randomX,
+    var c = baddie1.create(game.world.randomX -20,
                            (Math.random() * 150) + 50,
-                           'baddie1', game.rnd.integerInRange(0, 40));
+                           'baddie1', game.rnd.integerInRange(0, 20));
     c.name = 'bad' + i;
     c.body.immovable = true;
   }
@@ -83,6 +83,7 @@ function update() {
 
   // //check for input to move player
   player.body.velocity.x = 0;
+  player.body.velocity.y = 0;
 
   if (cursors.left.isDown) {
     player.body.velocity.x = -200;
@@ -129,9 +130,8 @@ function update() {
   function collisionHandler (bullet, baddie1) {
     bullet.kill();
     baddie1.kill();
-
     //  Add and update the score
-    score += 10;
+    score += 50;
     scoreText.text = 'Score: ' + score;
   }
 
