8b77fc9089e8f84a4b5a15ec09489b3e1c749150
jacobdlewis <jacobdlewis@gmail.com>
2015-03-25 01:47:08 -0500

    high scores update in realtime

--- app/js/gameOver.js
+++ app/js/gameOver.js
@@ -1,28 +1,9 @@
 (function(){
   game.state.add('gameOver', {create:create});
 
-  // var addScore;
-  var topTenScores;
-  getScoresFromFirebase();
 
   function create(){
-    console.log(topTenScores);
-    game.add.text(100, 120, 'HIGH SCORE LEADERBOARD', { fontSize: '48px', fill: 'white' });
-    game.add.text(200, 170, topTenScores[0].initials + '     ' + topTenScores[0].score, { fontSize: '32px', fill: 'white' });
-    game.add.text(200, 200, topTenScores[1].initials + '     ' + topTenScores[1].score, { fontSize: '32px', fill: 'white' });
-    game.add.text(200, 230, topTenScores[2].initials + '     ' + topTenScores[2].score, { fontSize: '32px', fill: 'white' });
-    game.add.text(200, 260, topTenScores[3].initials + '     ' + topTenScores[3].score, { fontSize: '32px', fill: 'white' });
-    game.add.text(200, 290, topTenScores[4].initials + '     ' + topTenScores[4].score, { fontSize: '32px', fill: 'white' });
-    game.add.text(200, 320, topTenScores[5].initials + '     ' + topTenScores[5].score, { fontSize: '32px', fill: 'white' });
-    game.add.text(200, 350, topTenScores[6].initials + '     ' + topTenScores[6].score, { fontSize: '32px', fill: 'white' });
-    game.add.text(200, 380, topTenScores[7].initials + '     ' + topTenScores[7].score, { fontSize: '32px', fill: 'white' });
-    game.add.text(200, 410, topTenScores[8].initials + '     ' + topTenScores[8].score, { fontSize: '32px', fill: 'white' });
-    game.add.text(200, 440, topTenScores[9].initials + '     ' + topTenScores[9].score, { fontSize: '32px', fill: 'white' });
-    game.add.text(130, 490, '- press SPACE to restart -', { fontSize: '32px', fill: 'white' });
-
-    //addScore();
-    var spacebar = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
-    spacebar.onDown.add(restartGame);
+    getScoresFromFirebase();
   }
 
   function restartGame() {
@@ -34,10 +15,26 @@
   fb.once('value', function(snapshot) {
     var storedScores = snapshot.val();
     var sortedScores =  _.sortBy(storedScores, 'score');
-    topTenScores = sortedScores.slice(sortedScores.length -10, sortedScores.length).reverse();
-    _.forEach(topTenScores, function(scoreObj) {
-    console.log(scoreObj.initials + ": " + scoreObj.score);
-    });
+    leaderboard = sortedScores.slice(sortedScores.length -10, sortedScores.length).reverse();
+    // _.forEach(leaderboard, function(scoreObj) {
+    // console.log(scoreObj.initials + ": " + scoreObj.score);
+    // });
+    game.add.text(100, 120, 'HIGH SCORE LEADERBOARD', { fontSize: '48px', fill: 'white' });
+    game.add.text(200, 170, " 1.  " + leaderboard[0].initials + '     ' + leaderboard[0].score, { fontSize: '32px', fill: 'white' });
+    game.add.text(200, 200, " 2.  " +leaderboard[1].initials + '     ' + leaderboard[1].score, { fontSize: '32px', fill: 'white' });
+    game.add.text(200, 230, " 3.  " +leaderboard[2].initials + '     ' + leaderboard[2].score, { fontSize: '32px', fill: 'white' });
+    game.add.text(200, 260, " 4.  " +leaderboard[3].initials + '     ' + leaderboard[3].score, { fontSize: '32px', fill: 'white' });
+    game.add.text(200, 290, " 5.  " +leaderboard[4].initials + '     ' + leaderboard[4].score, { fontSize: '32px', fill: 'white' });
+    game.add.text(200, 320, " 6.  " +leaderboard[5].initials + '     ' + leaderboard[5].score, { fontSize: '32px', fill: 'white' });
+    game.add.text(200, 350, " 7.  " +leaderboard[6].initials + '     ' + leaderboard[6].score, { fontSize: '32px', fill: 'white' });
+    game.add.text(200, 380, " 8.  " +leaderboard[7].initials + '     ' + leaderboard[7].score, { fontSize: '32px', fill: 'white' });
+    game.add.text(200, 410, " 9.  " +leaderboard[8].initials + '     ' + leaderboard[8].score, { fontSize: '32px', fill: 'white' });
+    game.add.text(200, 440, "10. " +leaderboard[9].initials + '     ' + leaderboard[9].score, { fontSize: '32px', fill: 'white' });
+    game.add.text(130, 490, '- press SPACE to restart -', { fontSize: '32px', fill: 'white' });
+
+    //addScore();
+    var spacebar = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+    spacebar.onDown.add(restartGame);
   });
 }
 })();
